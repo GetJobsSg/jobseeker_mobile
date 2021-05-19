9c9026327ee57c405f20bbe15d36b92f7faf2c3a
BENSTOH <benson.toh@daimler.com>
2021-05-19 21:35:28 +0800

    chore: rename bundle id and app name

--- android/app/src/main/java/com/getjobs/jobseeker/MainActivity.java
+++ android/app/src/main/java/com/getjobs/jobseeker/MainActivity.java
@@ -1,4 +1,4 @@
-package com.jobseeker_app;
+package com.getjobs.jobseeker;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "jobseeker_app";
+    return "GetJobs";
   }
 }
--- ios/GetJobs/AppDelegate.m
+++ ios/GetJobs/AppDelegate.m
@@ -33,7 +33,7 @@ static void InitializeFlipper(UIApplication *application) {
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"jobseeker_app"
+                                                   moduleName:@"GetJobs"
                                             initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
