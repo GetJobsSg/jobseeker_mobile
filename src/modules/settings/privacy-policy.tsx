0320629330405e95e7b24a4bcde67ad13d04e04f
benson7667 <benson_7667@hotmail.com>
2021-12-13 22:14:00 +0800

    feat: html text for privacy policy

--- src/modules/settings/privacy-policy.tsx
+++ src/modules/settings/privacy-policy.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
-import PP from './pp.html';
+import { useNavigation } from '@react-navigation/native';
+import { FixedScreen, Header, IconButton } from '../../components';
+import PrivacyHtml from './privacy-policy.html';
 
-const PrivacyPolicyScreen = () => <WebView source={{ uri: PP }} />;
+// const PrivacyPolicyScreen = () => <WebView source={{ uri: PP }} />;
+const PrivacyPolicyScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <FixedScreen
+      px={0}
+      appBar={
+        <Header
+          title="Privacy Policy"
+          leftIcon={<IconButton icon="circle_back_btn" onPress={() => navigation.goBack()} />}
+        />
+      }
+    >
+      <WebView source={PrivacyHtml} />
+    </FixedScreen>
+  );
+};
 
 export default PrivacyPolicyScreen;
--- src/modules/settings/term-of-use.tsx
+++ src/modules/settings/term-of-use.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { WebView } from 'react-native-webview';
+import PrivacyHtml from './privacy-policy.html';
+import { FixedScreen, Header, IconButton } from '../../components';
 
-const TermOfUseScreen = () => <WebView source={{ uri: 'https://reactnative.dev/' }} />;
+// const TermOfUseScreen = () => <WebView source={{ uri: 'https://reactnative.dev/' }} />;
+const TermOfUseScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <FixedScreen
+      px={0}
+      appBar={
+        <Header
+          title="Privacy Policy"
+          leftIcon={<IconButton icon="circle_back_btn" onPress={() => navigation.goBack()} />}
+        />
+      }
+    >
+      <WebView source={PrivacyHtml} />
+    </FixedScreen>
+  );
+};
 
 export default TermOfUseScreen;
