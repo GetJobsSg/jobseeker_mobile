201a1bcdd7c259d862f97d62c26323561c0be128
benson7667 <benson_7667@hotmail.com>
2022-01-10 23:37:01 +0800

    chore: update google service json

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 const config = {
   networkTimeout: 10000,
-  apiHost: 'http://getjobs-dev-backend.eba-iukcicq5.ap-southeast-1.elasticbeanstalk.com/',
-  // apiHost: 'http://localhost:8080/',
+  // apiHost: 'http://getjobs-dev-backend.eba-iukcicq5.ap-southeast-1.elasticbeanstalk.com/',
+  apiHost: 'https://getjobs-prod-backend.getjobsfor.me/',
 };
 
 export default config;
